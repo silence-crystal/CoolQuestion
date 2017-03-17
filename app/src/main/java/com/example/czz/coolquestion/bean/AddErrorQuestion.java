c5c7d7c875f726291e1875df075417eb4a891368
wyx770927314 <770927314@qq.com>
2017-03-17 11:29:21 +0800

    xiu gai bufen UI

---
+++ app/src/main/java/com/example/czz/coolquestion/bean/AddErrorQuestion.java
@@ -0,0 +1,32 @@
+package com.example.czz.coolquestion.bean;
+
+/**
+ * Created by dell on 2017/3/17.
+ */
+
+public class AddErrorQuestion {
+
+    /**
+     * reason :
+     * result : failed
+     */
+
+    private String reason;
+    private String result;
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
